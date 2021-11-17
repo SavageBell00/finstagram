6e6cf08d335bfd993ce7e88aeb3a70a5aa536363
SavageBell <dsavb@pm.me>
2021-11-17 18:02:57 +0000

    W2D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,4 @@
+
 def humanized_time_ago(time_ago_in_minutes)
     if time_ago_in_minutes >= 60
         "#{time_ago_in_minutes / 60} hours ago"
@@ -8,17 +9,19 @@ end
 
 get '/' do
     # File.read(File.join('app/views', 'index.html'))
-    
-   username = "sharky_j"
-   avatar_url = "http://naserca.com/images/sharky_j.jpg"
-   photo_url = "http://naserca.com/images/shark.jpg"
-   time_ago_in_minutes = 15
-   like_count = 0
-   comment_count = 1
-   comments = [
-       "sharky_j: Out for the long weekend... too embarrassed to show y'll the beach bod!"
-   ]
-
-   humanized_time_ago(time_ago_in_minutes)
+ 
+    finstagram_post = {
+        username: "sharky_j",
+        avatar_url: "http://naserca.com/images/sharky_j.jpg",
+        photo_url: "http://naserca.com/images/shark.jpg",
+        time_ago_in_minutes: 15,
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "sharky_j",
+            text: "Out for the long weekend...too embarrassed to show y'all the beach bod!"
+        }]
+    }
    
+    humanized_time_ago(finstagram_post[:time_ago_in_minutes])
 end
