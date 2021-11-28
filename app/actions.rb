1678d6970ad42f1a494b3aab3d820a778e6399cf
SavageBell <dsavb@pm.me>
2021-11-28 23:51:08 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,18 +1,7 @@
-
-def humanized_time_ago(time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes / 60} hours ago"
-    else
-        "#{time_ago_in_minutes} minutes ago"
-    end
-end
-
 get '/' do
-    # File.read(File.join('app/views', 'index.html'))
     @finstagram_posts = FinstagramPost.order(created_at: :desc)    
     erb(:index)
 end
-
 
 get '/signup' do
     @user = User.new
@@ -28,7 +17,7 @@ post '/signup' do
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
     
     if @user.save
-    "User #{username} saved!"
+        "User #{username} saved!"
     else
         erb(:signup)
     end
